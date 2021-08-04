c332a3fb4923e5c9228f549ba72a3d4c894aba90
Jeff Jessie <j3ffjessie@protonmail.com>
2021-08-03 19:34:45 -0500

    Add basic form setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import {makeStyles} from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 const GradeCheck = () => {
+
+  const classes = useStyles();
+  const currentGrade= '';
+  const gradeWanted='';
+
   return (
-    <div>
-      
-    </div>
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField required id="standard-required" label="Your Current Grade % "  />
+      <TextField required id="standard-required" label="The Grade You Want." /> 
+    </form>
   );
 };
 
