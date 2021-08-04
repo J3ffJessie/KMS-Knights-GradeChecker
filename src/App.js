649d060d1ee70ab086d0d576af12493c39f23ba1
j3ffjessie <jejess6125@gmail.com>
2021-08-04 14:23:02 -0500

    Basic styling and adding in classes for styling.  Implemented beginning stages of state management and onChange handler.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from '@material-ui/core/styles';
-
+import { useState } from 'react';
+import { Container, RadioGroup } from '@material-ui/core';
+import { findByLabelText } from '@testing-library/react';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
       width: '25ch',
+      position:'relative',
     },
+  },
+  form: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    paddingTop: '100px',
   },
 }));
 
 const GradeCheck = () => {
 
   const classes = useStyles();
-  const currentGrade= '';
-  const gradeWanted='';
+
+  const [currentGrade, setCurrentGrade] = useState('');
+  const [gradeWanted, setGradeWanted] = useState('');
+  const [gradeWeight, setGradeWeight] = useState('');
+  const [finalGrade, setFinalGrade] = useState('');
+
+  function calculateGrade(currentGrade, gradeWanted, gradeWeight) {
+    let final = (gradeWanted -currentGrade*(100.0 - gradeWeight)/ 100.0 / (gradeWeight/ 100.0));
+    setFinalGrade(final.toFixed(2));
+  }
+
+  const handleChange = (event) => {
+    event.preventDefault();
+  }
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
-      <TextField required id="standard-required" label="Your Current Grade % "  />
-      <TextField required id="standard-required" label="The Grade You Want." /> 
+    <Container className={classes.root} display="flex">
+    <form className={classes.form} noValidate autoComplete="off" justifyContent="center">
+      <TextField required id="standard-required" label="Your Current Grade % " onChange= {(event) => setCurrentGrade(event.target.value)} />
+      <TextField required id="standard-required" label="The Grade You Want." onChange = {(event) => setGradeWanted(event.target.value)} /> 
+
+      <RadioGroup>
+
+      </RadioGroup>
     </form>
+    </Container>
   );
 };
 
