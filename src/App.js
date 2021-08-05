fdb3b6a5265ce725919ca178200072fa39f7a541
j3ffjessie <jejess6125@gmail.com>
2021-08-04 20:06:05 -0500

    Do up the main functionality for the respective inputs and buttons

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import {makeStyles} from '@material-ui/core/styles';
 import { useState } from 'react';
-import { Container, RadioGroup } from '@material-ui/core';
-import { findByLabelText } from '@testing-library/react';
+import { Button, ButtonGroup, Container, RadioGroup } from '@material-ui/core';
+import { FormControlLabel } from '@material-ui/core';
+import { Radio } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,7 +22,21 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'column',
-    paddingTop: '100px',
+    paddingTop: '80px',
+  },
+  radio: {
+    display: 'flex',
+    justifyContent:'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  buttonGroup: {
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingTop: '25px',
+    width: 'auto',
   },
 }));
 
@@ -29,16 +47,23 @@ const GradeCheck = () => {
   const [currentGrade, setCurrentGrade] = useState('');
   const [gradeWanted, setGradeWanted] = useState('');
   const [gradeWeight, setGradeWeight] = useState('');
-  const [finalGrade, setFinalGrade] = useState('');
+  const [finalGrade, setFinalGrade] = useState();
 
-  function calculateGrade(currentGrade, gradeWanted, gradeWeight) {
+  // const handleChange = (event) => {
+  //   event.preventDefault();
+  // }
+
+  function checkGrade(currentGrade, gradeWanted, gradeWeight) {
     let final = (gradeWanted -currentGrade*(100.0 - gradeWeight)/ 100.0 / (gradeWeight/ 100.0));
     setFinalGrade(final.toFixed(2));
+  };
+
+  function handleSubmit() {
+    checkGrade();
+    console.log(finalGrade);
   }
 
-  const handleChange = (event) => {
-    event.preventDefault();
-  }
+
 
   return (
     <Container className={classes.root} display="flex">
@@ -46,10 +71,26 @@ const GradeCheck = () => {
       <TextField required id="standard-required" label="Your Current Grade % " onChange= {(event) => setCurrentGrade(event.target.value)} />
       <TextField required id="standard-required" label="The Grade You Want." onChange = {(event) => setGradeWanted(event.target.value)} /> 
 
-      <RadioGroup>
-
+      <RadioGroup className={classes.radio} >
+      <FormControlLabel value="Test/Quiz" control={<Radio />} label="Test/Quiz" onClick={(event) => setGradeWeight('70')} />
+      <FormControlLabel value="Classwork/Homework" control={<Radio />} label="Class/Homework" onClick={(event) => setGradeWeight('30')} />
       </RadioGroup>
     </form>
+
+    <ButtonGroup className={classes.buttonGroup}>
+    <Button variant='contained' color='primary' onClick={handleSubmit}>Check Grade</Button>
+
+    <Button variant='contained' color='secondary'>Reset</Button>
+    </ButtonGroup>
+
+    <Box>
+      <Typography variant='h3' component='h5' className={classes.form}>
+        You Need To Score : 
+      </Typography>
+      <Typography variant='h5' component='h6' className={classes.form}>
+        {finalGrade}
+      </Typography>
+    </Box>
     </Container>
   );
 };
